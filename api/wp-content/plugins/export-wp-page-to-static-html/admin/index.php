60629caada7a5c7d40e61d813f342edcc428866f
TangWulin6 <HMZ1547528894@163.com>
2025-11-09 19:54:16 +0800

    delete

--- api/wp-content/plugins/export-wp-page-to-static-html/admin/index.php
+++
@@ -1,4 +0,0 @@
-<?php // Silence is golden
-
-$wpDir = str_replace('wp-content\plugins\export-wp-page-to-static-html-pro-master\admin', '', __DIR__);
-require($wpDir.'wp-load.php');
--- api/wp-content/plugins/simply-static/src/admin/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'c915e0dec29cdfc31f45');
